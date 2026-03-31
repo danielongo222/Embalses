7891c06297d902296559ced67356f984b0bc840a
danielongo222 <daniel.mediano@gmail.com>
2026-03-31 09:04:40 +0200

    Update procesar.py

--- procesar.py
+++ procesar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Scraping de embalses.net para obtener datos reales.
-Datos: Ministerio de Transición Ecológica, CC BY 4.0, via embalses.net
+Obtiene datos de embalses.net guardando las URLs completas desde las páginas
+de cuenca, luego extrae datos de cada embalse individual.
 """
 import json, re, time, datetime, sys, urllib.request, gzip as gzip_mod
  
@@ -19,7 +19,6 @@ META = {
     "proserpina":            ("Guadiana",                  "Badajoz",       "Extremadura"),
     "cornalvo":              ("Guadiana",                  "Badajoz",       "Extremadura"),
     "el vicario":            ("Guadiana",                  "Ciudad Real",   "Castilla-La Mancha"),
-    "peñarroya":             ("Guadiana",                  "Ciudad Real",   "Castilla-La Mancha"),
     "alcantara":             ("Tajo",                      "Cáceres",       "Extremadura"),
     "valdecanas":            ("Tajo",                      "Cáceres",       "Extremadura"),
     "gabriel y galan":       ("Tajo",                      "Cáceres",       "Extremadura"),
@@ -38,133 +37,73 @@ META = {
     "riaño":                 ("Duero",                     "León",          "Castilla y León"),
     "barrios de luna":       ("Duero",                     "León",          "Castilla y León"),
     "villalcampo":           ("Duero",                     "Zamora",        "Castilla y León"),
-    "castro":                ("Duero",                     "Zamora",        "Castilla y León"),
-    "linares del arroyo":    ("Duero",                     "Segovia",       "Castilla y León"),
     "aguilar de campoo":     ("Duero",                     "Palencia",      "Castilla y León"),
-    "cervera":               ("Duero",                     "Palencia",      "Castilla y León"),
     "requejada":             ("Duero",                     "Palencia",      "Castilla y León"),
     "compuerto":             ("Duero",                     "Palencia",      "Castilla y León"),
     "cuerda del pozo":       ("Duero",                     "Soria",         "Castilla y León"),
     "santa teresa":          ("Duero",                     "Salamanca",     "Castilla y León"),
-    "burgomillodo":          ("Duero",                     "Segovia",       "Castilla y León"),
     "mequinenza":            ("Ebro",                      "Zaragoza",      "Aragón"),
     "yesa":                  ("Ebro",                      "Zaragoza",      "Aragón"),
     "mediano":               ("Ebro",                      "Huesca",        "Aragón"),
     "ribarroja":             ("Ebro",                      "Tarragona",     "Cataluña"),
     "itoiz":                 ("Ebro",                      "Navarra",       "Navarra"),
     "el grado":              ("Ebro",                      "Huesca",        "Aragón"),
-    "vadiello":              ("Ebro",                      "Huesca",        "Aragón"),
     "canelles":              ("Ebro",                      "Huesca",        "Aragón"),
-    "santa ana":             ("Ebro",                      "Huesca",        "Aragón"),
-    "bubal":                 ("Ebro",                      "Huesca",        "Aragón"),
-    "lanuza":                ("Ebro",                      "Huesca",        "Aragón"),
     "sotonera":              ("Ebro",                      "Huesca",        "Aragón"),
-    "alloz":                 ("Ebro",                      "Navarra",       "Navarra"),
-    "eugui":                 ("Ebro",                      "Navarra",       "Navarra"),
-    "irabia":                ("Ebro",                      "Navarra",       "Navarra"),
     "ullibarri-gamboa":      ("Ebro",                      "Álava",         "País Vasco"),
-    "urrunaga":              ("Ebro",                      "Álava",         "País Vasco"),
     "mansilla":              ("Ebro",                      "La Rioja",      "La Rioja"),
-    "gonzalez lacasa":       ("Ebro",                      "La Rioja",      "La Rioja"),
-    "pajares":               ("Ebro",                      "La Rioja",      "La Rioja"),
     "ebro":                  ("Ebro",                      "Cantabria",     "Cantabria"),
     "iznajar":               ("Guadalquivir",              "Córdoba",       "Andalucía"),
     "tranco de beas":        ("Guadalquivir",              "Jaén",          "Andalucía"),
     "guadalmena":            ("Guadalquivir",              "Jaén",          "Andalucía"),
     "bembezar":              ("Guadalquivir",              "Córdoba",       "Andalucía"),
-    "malpasillo":            ("Guadalquivir",              "Córdoba",       "Andalucía"),
     "la breña ii":           ("Guadalquivir",              "Córdoba",       "Andalucía"),
-    "la breña":              ("Guadalquivir",              "Córdoba",       "Andalucía"),
     "puente nuevo":          ("Guadalquivir",              "Córdoba",       "Andalucía"),
-    "el carpio":             ("Guadalquivir",              "Córdoba",       "Andalucía"),
-    "encinarejo":            ("Guadalquivir",              "Córdoba",       "Andalucía"),
     "jandula":               ("Guadalquivir",              "Jaén",          "Andalucía"),
     "el rumblar":            ("Guadalquivir",              "Jaén",          "Andalucía"),
-    "doña aldonza":          ("Guadalquivir",              "Jaén",          "Andalucía"),
     "giribaile":             ("Guadalquivir",              "Jaén",          "Andalucía"),
-    "colomera":              ("Guadalquivir",              "Granada",       "Andalucía"),
-    "cubillas":              ("Guadalquivir",              "Granada",       "Andalucía"),
-    "francisco abellan":     ("Guadalquivir",              "Granada",       "Andalucía"),
     "rules":                 ("Guadalquivir",              "Granada",       "Andalucía"),
     "el pintado":            ("Guadalquivir",              "Sevilla",       "Andalucía"),
     "aracena":               ("Guadalquivir",              "Huelva",        "Andalucía"),
     "zufre":                 ("Guadalquivir",              "Huelva",        "Andalucía"),
-    "agrio":                 ("Guadalquivir",              "Sevilla",       "Andalucía"),
-    "hueznar":               ("Guadalquivir",              "Sevilla",       "Andalucía"),
-    "retortillo":            ("Guadalquivir",              "Sevilla",       "Andalucía"),
     "guadalcacin":           ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
     "bornos":                ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
-    "arcos":                 ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
-    "zahara-el gastor":      ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
     "zahara":                ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
     "barbate":               ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
     "celemin":               ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
-    "majaceite":             ("Guadalete-Barbate",         "Cádiz",         "Andalucía"),
     "canales":               ("C. Mediterráneas Andaluzas","Granada",       "Andalucía"),
     "beninar":               ("C. Mediterráneas Andaluzas","Almería",       "Andalucía"),
     "la viñuela":            ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
-    "el limonero":           ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
-    "conde del guadalhorce": ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
     "guadalteba":            ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
-    "guadalhorce":           ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
-    "la concepcion":         ("C. Mediterráneas Andaluzas","Málaga",        "Andalucía"),
     "andevalo":              ("Tinto-Odiel-Piedras",       "Huelva",        "Andalucía"),
-    "sancho el mayor":       ("Tinto-Odiel-Piedras",       "Huelva",        "Andalucía"),
     "contreras":             ("Júcar",                     "Cuenca",        "Castilla-La Mancha"),
     "tous":                  ("Júcar",                     "Valencia",      "Comunitat Valenciana"),
     "alarcon":               ("Júcar",                     "Cuenca",        "Castilla-La Mancha"),
     "benageber":             ("Júcar",                     "Valencia",      "Comunitat Valenciana"),
-    "loriguilla":            ("Júcar",                     "Valencia",      "Comunitat Valenciana"),
-    "forata":                ("Júcar",                     "Valencia",      "Comunitat Valenciana"),
-    "amadorio":              ("Júcar",                     "Alicante",      "Comunitat Valenciana"),
-    "el talave":             ("Segura",                    "Albacete",      "Castilla-La Mancha"),
     "cenajo":                ("Segura",                    "Murcia",        "Murcia"),
-    "camarillas":            ("Segura",                    "Murcia",        "Murcia"),
     "fuensanta":             ("Segura",                    "Murcia",        "Murcia"),
     "la pedrera":            ("Segura",                    "Murcia",        "Murcia"),
-    "alfonso xiii":          ("Segura",                    "Murcia",        "Murcia"),
-    "puentes":               ("Segura",                    "Murcia",        "Murcia"),
-    "frieira":               ("Miño-Sil",                  "Ourense",       "Galicia"),
-    "peares":                ("Miño-Sil",                  "Ourense",       "Galicia"),
     "belesar":               ("Miño-Sil",                  "Lugo",          "Galicia"),
     "das conchas":           ("Miño-Sil",                  "Ourense",       "Galicia"),
-    "chandrexa":             ("Miño-Sil",                  "Ourense",       "Galicia"),
-    "velle":                 ("Miño-Sil",                  "Ourense",       "Galicia"),
-    "castrelo":              ("Miño-Sil",                  "Ourense",       "Galicia"),
     "cecebre":               ("Galicia Costa",             "A Coruña",      "Galicia"),
-    "baña":                  ("Galicia Costa",             "A Coruña",      "Galicia"),
     "portodemouros":         ("Galicia Costa",             "A Coruña",      "Galicia"),
     "eiras":                 ("Galicia Costa",             "Pontevedra",    "Galicia"),
-    "urdalur":               ("Cantábrico",                "Álava",         "País Vasco"),
     "añarbe":                ("Cantábrico",                "Guipúzcoa",     "País Vasco"),
     "la cohilla":            ("Cantábrico",                "Cantabria",     "Cantabria"),
     "la baells":             ("Cuencas Internas Cataluña", "Barcelona",     "Cataluña"),
     "sau":                   ("Cuencas Internas Cataluña", "Barcelona",     "Cataluña"),
     "susqueda":              ("Cuencas Internas Cataluña", "Girona",        "Cataluña"),
-    "darnius-boadella":      ("Cuencas Internas Cataluña", "Girona",        "Cataluña"),
-    "sant ponç":             ("Cuencas Internas Cataluña", "Lleida",        "Cataluña"),
     "oliana":                ("Cuencas Internas Cataluña", "Lleida",        "Cataluña"),
     "rialb":                 ("Cuencas Internas Cataluña", "Lleida",        "Cataluña"),
-    "terradets":             ("Cuencas Internas Cataluña", "Lleida",        "Cataluña"),
-    "camarasa":              ("Cuencas Internas Cataluña", "Lleida",        "Cataluña"),
 }
  
 CUENCAS_URLS = [
-    "cuenca-1-segura.html",
-    "cuenca-2-duero.html",
-    "cuenca-3-tajo.html",
-    "cuenca-4-guadalquivir.html",
-    "cuenca-5-ebro.html",
-    "cuenca-6-guadiana.html",
-    "cuenca-7-jucar.html",
-    "cuenca-8-mino-sil.html",
-    "cuenca-9-med-andaluza.html",
-    "cuenca-10-galicia-costa.html",
-    "cuenca-11-cataluna-interna.html",
-    "cuenca-16-cantabrico-oriental.html",
-    "cuenca-17-cantabrico-occidental.html",
-    "cuenca-18-tinto-odiel-y-piedras.html",
-    "cuenca-19-guadalete-barbate.html",
+    "cuenca-1-segura.html", "cuenca-2-duero.html", "cuenca-3-tajo.html",
+    "cuenca-4-guadalquivir.html", "cuenca-5-ebro.html", "cuenca-6-guadiana.html",
+    "cuenca-7-jucar.html", "cuenca-8-mino-sil.html", "cuenca-9-med-andaluza.html",
+    "cuenca-10-galicia-costa.html", "cuenca-11-cataluna-interna.html",
+    "cuenca-16-cantabrico-oriental.html", "cuenca-17-cantabrico-occidental.html",
+    "cuenca-18-tinto-odiel-y-piedras.html", "cuenca-19-guadalete-barbate.html",
     "cuenca-12-pais-vasco-interna.html",
 ]
  
@@ -199,11 +138,9 @@ def fetch(url):
  
 def p_num(s):
     if not s: return None
-    s = s.strip().replace("\xa0","").replace(" ","")
-    # 1.705 (miles con punto, sin decimal) → 1705
+    s = s.strip().replace("\xa0","").replace(" ","").replace("\u200b","")
     if re.match(r'^\d{1,3}(\.\d{3})+$', s):
         s = s.replace(".","")
-    # 1.013,45 → 1013.45
     elif re.match(r'^\d{1,3}(\.\d{3})+(,\d+)?$', s):
         s = s.replace(".","").replace(",",".")
     else:
@@ -212,15 +149,12 @@ def p_num(s):
     except: return None
  
 def scrape_cuenca(slug):
-    """Devuelve lista de (id, url_completa) de pantanos en la cuenca."""
     url = f"{BASE}/{slug}"
     try:
         html = fetch(url)
     except Exception as e:
         print(f"  Error {slug}: {e}")
         return []
-    # Las URLs en las páginas de cuenca son del tipo:
-    # href="/pantano-931-buendia.html" o href="pantano-931-buendia.html"
     matches = re.findall(
         r'href=["\'](?:https?://www\.embalses\.net)?(/pantano-(\d+)-[^"\']+\.html)["\']',
         html)
@@ -228,15 +162,20 @@ def scrape_cuenca(slug):
     for path, pid in matches:
         if pid not in seen:
             seen[pid] = f"{BASE}{path}"
-    return list(seen.items())  # [(id_str, url), ...]
+    return list(seen.items())
  
 def scrape_pantano(pid, url):
     try:
         html = fetch(url)
-    except Exception:
+    except Exception as e:
         return None
  
-    # Nombre: buscar "Embalse: NombreXxx" en el texto
+    # ── DIAGNÓSTICO: imprimir las primeras líneas de la primera pantalla ──
+    # (solo para los primeros 3, para no llenar el log)
+    if int(pid) < 40:
+        snippet = html[:800].replace('\n',' ')
+        print(f"  [DEBUG pid={pid}] {snippet[:300]}")
+ 
     nombre_m = re.search(r'Embalse:\s*([^\n(<]+?)(?:\s*\(Tiempo Real\)|\s*\(Caudal)', html)
     if not nombre_m:
         nombre_m = re.search(r'<h1[^>]*>(?:Pantano|Embalse|Presa)\s+([^<]+)</h1>', html, re.I)
@@ -244,47 +183,31 @@ def scrape_pantano(pid, url):
         return None
     nombre = nombre_m.group(1).strip()
  
-    # Agua embalsada: número en negrita seguido de hm3
-    # HTML real: **1.013**\n\n**hm3**
-    agua_m = re.search(
-        r'Agua embalsada[^:]*:\*{0,2}\s*\n?\s*\*{0,2}([\d.,]+)\*{0,2}\s*\n?\s*\*{0,2}\s*\n?\s*hm',
-        html, re.I)
-    if not agua_m:
-        # Buscar patrón en el markdown renderizado: número solo, luego hm3
-        agua_m = re.search(
-            r'\*\*([\d.,]+)\*\*\s*\n+\s*\*\*hm3?\*\*',
-            html)
-    if not agua_m:
-        agua_m = re.search(r'([\d]{3,}(?:[.,]\d+)?)\s*\n\s*hm3\b', html)
- 
+    agua_m  = re.search(r'\*\*([\d.,]+)\*\*\s*\n+\s*\*\*hm3?\*\*', html)
     cap_m   = re.search(r'Capacidad:\s*\n?\s*([\d.,]+)\s*\n?\s*hm', html, re.I)
     hace_m  = re.search(r'Misma Semana \(\d{4}\):\s*\n?\s*([\d.,]+)\s*\n?\s*hm', html, re.I)
     med_m   = re.search(r'Misma Semana \(Med\. 10 A[ñn]os\):\s*\n?\s*([\d.,]+)\s*\n?\s*hm', html, re.I)
  
     hm3     = p_num(agua_m.group(1)) if agua_m else None
     cap     = p_num(cap_m.group(1))  if cap_m  else None
-    hace1y  = p_num(hace_m.group(1)) if hace_m  else None
-    media10 = p_num(med_m.group(1))  if med_m   else None
+    hace1y  = p_num(hace_m.group(1)) if hace_m else None
+    media10 = p_num(med_m.group(1))  if med_m  else None
  
     if not hm3 or not cap or cap < 1:
         return None
  
     cuenca, provincia, ccaa = buscar_meta(nombre)
     return {
-        "nombre":    nombre,
-        "cuenca":    cuenca,
-        "ccaa":      ccaa,
-        "provincia": provincia,
-        "capacidad": round(cap, 1),
-        "hm3":       round(hm3, 1),
+        "nombre": nombre, "cuenca": cuenca, "ccaa": ccaa, "provincia": provincia,
+        "capacidad": round(cap, 1), "hm3": round(hm3, 1),
         "hace1year": round(hace1y,  1) if hace1y  else round(hm3 * 0.85, 1),
         "media10":   round(media10, 1) if media10 else round(hm3 * 0.80, 1),
-        "fecha":     TODAY,
+        "fecha": TODAY,
     }
  
 def main():
     print("Obteniendo lista de embalses por cuencas…")
-    all_pantanos = {}  # id -> url
+    all_pantanos = {}
     for slug in CUENCAS_URLS:
         items = scrape_cuenca(slug)
         print(f"  {slug}: {len(items)} embalses")
@@ -297,6 +220,10 @@ def main():
     if not all_pantanos:
         print("ERROR: No se encontraron IDs", file=sys.stderr)
         sys.exit(1)
+ 
+    # Imprimir las primeras 3 URLs para diagnóstico
+    for i, (pid, url) in enumerate(list(all_pantanos.items())[:3]):
+        print(f"  [URL ejemplo] pid={pid} url={url}")
  
     embalses = []
     nombres_vistos = set()
@@ -316,22 +243,18 @@ def main():
         sys.exit(1)
  
     embalses.sort(key=lambda x: -x["capacidad"])
- 
     total = {
         "hm3":       round(sum(e["hm3"]       for e in embalses), 1),
         "capacidad": round(sum(e["capacidad"] for e in embalses), 1),
         "hace1year": round(sum(e["hace1year"] for e in embalses), 1),
         "media10":   round(sum(e["media10"]   for e in embalses), 1),
     }
- 
     datos = {"actualizado": TODAY, "total": total, "embalses": embalses}
     with open("datos.json", "w", encoding="utf-8") as f:
         json.dump(datos, f, ensure_ascii=False, indent=2)
  
     pct = round(total["hm3"] / total["capacidad"] * 100) if total["capacidad"] else 0
-    print(f"\n✓ datos.json: {len(embalses)} embalses")
-    print(f"  España: {total['hm3']} hm³ / {total['capacidad']} hm³ ({pct}%)")
-    print(f"  Fecha: {TODAY}")
+    print(f"\n✓ datos.json: {len(embalses)} embalses, {total['hm3']}/{total['capacidad']} hm³ ({pct}%)")
  
 if __name__ == "__main__":
     main()
